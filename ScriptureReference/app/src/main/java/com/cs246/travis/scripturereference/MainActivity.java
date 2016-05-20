84999ec03d8985c23e752d47da9efd397df58a1c
Travis Confer <Travis Confer>
2016-05-20 13:14:58 -0600

    First Commit

---
+++ ScriptureReference/app/src/main/java/com/cs246/travis/scripturereference/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cs246.travis.scripturereference;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button share = (Button) findViewById(R.id.button);
+        if (share != null)
+            share.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Results.class);
+                    String message = "Your favorite scripture is \n" +
+                            ((EditText) findViewById(R.id.editBook)).getText().toString()
+                            + " " +
+                            ((EditText) findViewById(R.id.editChapter)).getText().toString()
+                            + ":" +
+                            ((EditText) findViewById(R.id.editVerse)).getText().toString();
+                    intent.putExtra("message", message);
+                    startActivity(intent);
+                }
+            });
+    }
+}
---
+++ ScriptureReference/app/src/main/java/com/cs246/travis/scripturereference/Results.java
@@ -0,0 +1,32 @@
+package com.cs246.travis.scripturereference;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/*************************************
+ * Created by Travis on 5/20/2016.
+ ************************************/
+public class Results extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        displayText();
+    }
+
+    public void displayText(){
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        textView.setTextSize(40);
+        textView.setPadding(10,10,10,10);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+    }
+}
